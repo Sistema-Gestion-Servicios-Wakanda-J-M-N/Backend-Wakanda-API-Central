fadf7cca44fec783969c0a1a22d5146bb1e92e2e
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-11-26 12:46:31 +0100

    Arquitectura V1.0

---
+++ src/main/java/org/example/backendwakandaapicentral/BackendWakandaApiCentralApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaapicentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWakandaApiCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWakandaApiCentralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/backendwakandaapicentral/BackendWakandaApiCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendwakandaapicentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWakandaApiCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
