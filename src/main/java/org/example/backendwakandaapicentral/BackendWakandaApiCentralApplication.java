0506a032dc0e56f8554555fcc464efa88039d7c7
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-12-08 01:13:44 +0100

    conf-eureka

--- src/main/java/org/example/backendwakandaapicentral/BackendWakandaApiCentralApplication.java
+++ src/main/java/org/example/backendwakandaapicentral/BackendWakandaApiCentralApplication.java
@@ -2,8 +2,10 @@ package org.example.backendwakandaapicentral;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BackendWakandaApiCentralApplication {
 
 	public static void main(String[] args) {
